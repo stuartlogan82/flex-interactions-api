332270355f5a241cd3d2b206ef9bd93af9551fb6
stuartlogan82 <slogan@twilio.com>
2018-11-16 11:34:11 +0000

    Added templates for 404 and 500

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return '<h1>API Running!</h1>'
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
